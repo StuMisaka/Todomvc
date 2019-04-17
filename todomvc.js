4619f79b19306d2cb30495514eada03607ce990b
zhu <husteiczhu@163.com>
2019-04-17 23:53:57 +0800

    update todomvc

--- todomvc.js
+++ todomvc.js
@@ -2,14 +2,49 @@ var todomvc = {
   main:document.getElementById("main_list"),
   button_down:document.getElementById("button_down"),
   input:document.getElementById("input"),
-  data:[]
+  data:[],
 };
 function click_show(){
   alert("hello");
 }
 
-function click_status(){
-  alert("status changed");
+function getNumOfData(content_data){
+  var numOfData;
+  for(numOfData = 0 ; numOfData < todomvc.data.length ; numOfData++)
+  {
+    if(content_data === todomvc.data[numOfData].content)
+    {
+      return numOfData;
+    }
+  }
+  alert("Here is an error!");
+}
+
+function click_status(thisline_content_data){
+  var thisline = getNumOfData(thisline_content_data);
+  if(todomvc.data[thisline].status === "unfinished")
+  {
+    todomvc.data[thisline].status = "finished";
+    todomvc.data[thisline].status_list.innerHTML = "Y";
+    todomvc.data[thisline].p_list.className = "p_list_finished";
+  }
+  else if(todomvc.data[thisline].status === "finished")
+  {
+    todomvc.data[thisline].status = "unfinished";
+    todomvc.data[thisline].status_list.innerHTML = "N";
+    todomvc.data[thisline].p_list.className = "p_list_unfinished";
+  }
+}
+
+function click_delete(thisline_content_data){
+  var thisline = getNumOfData(thisline_content_data);
+  var main_list = todomvc.data[thisline].status_list.parentNode;
+  var main_list_length = parseInt(todomvc.main.style.height);
+  main_list.removeChild(todomvc.data[thisline].status_list);
+  main_list.removeChild(todomvc.data[thisline].div_list);
+  main_list.removeChild(todomvc.data[thisline].delete_list);
+  todomvc.data = todomvc.data.splice(thisline,1);
+  todomvc.main.style.height = (main_list_length - 6) + "%";
 }
 
 document.onkeyup = function (e) {
@@ -22,12 +57,16 @@ document.onkeyup = function (e) {
     var content_list = document.createTextNode(content_data);
     var status_list = document.createElement("div");
     var delete_list = document.createElement("div");
-    var data = {
+    var record = {
       "content":content_data,
-      "status":"unfinished"
+      "status":"unfinished",
+      "status_list":status_list,
+      "div_list":div_list,
+      "p_list": p_list ,
+      "delete_list":delete_list,
     }
     if(!content_data) return false;
-    todomvc.data.push(data);
+    todomvc.data.push(record);
     todomvc.main.style.height = (main_list_length + 6) + "%";
     todomvc.main.appendChild(status_list);
     todomvc.main.appendChild(div_list);
@@ -35,10 +74,18 @@ document.onkeyup = function (e) {
     todomvc.input.value = null;
     div_list.appendChild(p_list);
     div_list.className = 'div_list';
+    p_list.className = "p_list_unfinished";
     status_list.className = "status_list";
     delete_list.className = "delete_list";
     p_list.appendChild(content_list);   
-    status_list.addEventListener('click',click_status);
+    status_list.addEventListener('click',function(){
+      click_status(content_data);
+    });
+    delete_list.addEventListener('click',function(){
+      click_delete(content_data);
+    })
+    status_list.innerHTML = "N";
+    delete_list.innerHTML = "<";
   }
 }
 
