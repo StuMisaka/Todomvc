3a0d2b84518a4d3bbaa4d44bc605a8c80029e631
StuMisaka <husteiczhu@163.com>
2019-10-23 23:51:19 +0800

    :construction: coding on constroller and store

--- src/constroller.js
+++ src/constroller.js
@@ -9,25 +9,50 @@ export default class Constroller{
 
 
   }
+  /**
+   * 往todo列表中增加项目
+   * @param {string} title 
+   */
   addItem(title){
     this.store.insert({
       id:'',
-      title,
+      title:title,
       completed:false
     });
   }
+  /**
+   * 编辑todo列表中已经保存的项目
+   * @param {number} id
+   * @param {string} title
+   */
   editItemSaved(id,title){
-
+    if(title.length){
+      this.store.update({id,title},() => {
+        this.view.editItem(id,title);
+      });
+    }
+    else{
+      this.removeItem(id);
+    }
   }
+  /**
+   * 移除列表中已有的项目
+   * @param {number} id 
+   */
   removeItem(id){
-
-
+    this.store.remove({id},() => {
+      this.view.removeItem(id);
+    });
   }
+  /**
+   * 
+   * @param {*} id 
+   */
   toggleItem(id){
 
   }
   toggleAll(){
-    
+
   }
 
 }
--- src/store.js
+++ src/store.js
@@ -5,15 +5,39 @@ export default class Store{
     */
     const localStorage = window.localStorage;
 
+    let todo;
+    this.getLocalStorage = () => {
+
+    }
+    this.setLocalStorage = () => {
+
+    }
+
   }
   find(query,callback){
-
+    const todo = this.getLocalStorage();
   }
   update(update,callback) {
-    
+    const todo = this.getLocalStorage();
+    const id = update.id;
+    for(let i = todo.length;i > 0;i--){
+      if(todo[i].id === id){
+
+      }
+    }
+
+    if(callback){
+      callback();
+    }
   }
   insert(item,callback){
+    const todo = this.getLocalStorage();
+    todo.push(item);
+    this.setLocalStorage(todo);
 
+    if(callback){
+      callback();
+    }
   }
   remove(query,callback){
 
